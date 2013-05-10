e7b6ca3d9d5803e8a764029d3e7e82fc97938642
Oliver Klee <typo3-coding@oliverklee.de>
2013-05-10 18:49:50 +0200

    [CLEANUP] Reformat and clean up the Java source code.

--- src/project/at/ac/tuwien/infosys/www/phpparser/ParseNode.java
+++ src/project/at/ac/tuwien/infosys/www/phpparser/ParseNode.java
@@ -1,6 +1,6 @@
 /*
  * ParseNode.java
- * 
+ *
  * Copyright (C) 2005 Nenad Jovanovic
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,57 +18,75 @@
  *
  */
 
-
 package at.ac.tuwien.infosys.www.phpparser;
 
-import java.util.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * An interior or leaf (token) node in the parse tree. Not all methods are meaningful
- * for both interior and token nodes, so they may throw an 
+ * for both interior and token nodes, so they may throw an
  * <code>UnsupportedOperationException</code> in such cases.
- * 
- * @author  Nenad Jovanovic
- * @version July 14, 2005
+ *
+ * @author Nenad Jovanovic
  */
-public final class ParseNode
-implements Serializable {
-
-    /** symbol (token or rule) number */ 
+public final class ParseNode implements Serializable {
+    /**
+     * symbol (token or rule) number
+     */
     private final int symbol;
-    
-    /** name (useful for output) */
+
+    /**
+     * name (useful for output)
+     */
     private final String name;
 
-    /** name of the parsed file */
+    /**
+     * name of the parsed file
+     */
     private final String fileName;
-    
-    /** child nodes */
+
+    /**
+     * child nodes
+     */
     private List children;
-    
-    /** parent node; available for all nodes except the root of the parse tree */
+
+    /**
+     * parent node; available for all nodes except the root of the parse tree
+     */
     private ParseNode parent;
 
-    /** lexeme and line number; available for tokens only (i.e. parse tree leaves) */
+    /**
+     * lexeme; available for tokens only (i.e. parse tree leaves)
+     */
     private final String lexeme;
-    private final int lineno;
+    /**
+     * line number; available for tokens only (i.e. parse tree leaves)
+     */
+    private final int lineNumber;
 
-    /** node ID and static helper field for ID generation */
+    /**
+     * node ID for ID generation
+     */
     private final int id;
+    /**
+     * static helper field for ID generation
+     */
     private static int minFreeId = 0;
-    
-    /** is this node a token node? */
+
+    /**
+     * whether this node is a token node
+     */
     private boolean isToken;
 
-// CONSTRUCTORS ********************************************************************
- 
-    /** 
+    /**
      * Constructs an interior node.
-     * 
-     * @param symbol    symbol number 
-     * @param name      node name (useful for output)
-     * @param fileName  name of the parsed file
+     *
+     * @param symbol   symbol number
+     * @param name     node name (useful for output)
+     * @param fileName name of the parsed file
      */
     public ParseNode(int symbol, String name, String fileName) {
         this.symbol = symbol;
@@ -77,48 +95,45 @@ implements Serializable {
         this.children = new ArrayList();
         this.parent = null;
         this.lexeme = null;
-        this.lineno = -1;
+        this.lineNumber = -1;
         this.id = ParseNode.minFreeId++;
         this.isToken = false;
     }
- 
-    /** 
+
+    /**
      * Constructs a leaf (token) node.
-     * 
-     * @param symbol    symbol number 
-     * @param name      node name (useful for output)
-     * @param fileName  name of the parsed file
-     * @param lexeme    lexeme (the literal string in the parsed file) 
-     * @param lineno    the lexeme's line number in the parsed file
+     *
+     * @param symbol     symbol number
+     * @param name       node name (useful for output)
+     * @param fileName   name of the parsed file
+     * @param lexeme     lexeme (the literal string in the parsed file)
+     * @param lineNumber the lexeme's line number in the parsed file
      */
-    public ParseNode(int symbol, String name, String fileName, String lexeme, int lineno) {
+    public ParseNode(int symbol, String name, String fileName, String lexeme, int lineNumber) {
         this.symbol = symbol;
         this.name = name;
         this.fileName = fileName;
         this.children = Collections.EMPTY_LIST;
         this.parent = null;
         this.lexeme = lexeme;
-        this.lineno = lineno;
+        this.lineNumber = lineNumber;
         this.id = ParseNode.minFreeId++;
         this.isToken = true;
-    } 
-
-
-// GET *****************************************************************************
+    }
 
     /**
      * Returns this node's name.
-     * 
-     * @return  this node's name
+     *
+     * @return this node's name
      */
     public String getName() {
         return this.name;
     }
-    
+
     /**
      * Returns the name of the scanned file.
-     * 
-     * @return  the name of the scanned file
+     *
+     * @return the name of the scanned file
      */
     public String getFileName() {
         return this.fileName;
@@ -126,17 +141,17 @@ implements Serializable {
 
     /**
      * Returns this node's symbol number.
-     * 
-     * @return  this node's symbol number
+     *
+     * @return this node's symbol number
      */
-    public int getSymbol() {        
+    public int getSymbol() {
         return this.symbol;
-    } 
+    }
 
     /**
      * Returns this node's children.
-     * 
-     * @return  this node's children
+     *
+     * @return this node's children
      */
     public List getChildren() {
         return this.children;
@@ -144,8 +159,8 @@ implements Serializable {
 
     /**
      * Returns the number of children.
-     * 
-     * @return  the number of children
+     *
+     * @return the number of children
      */
     public int getNumChildren() {
         return this.children.size();
@@ -153,140 +168,119 @@ implements Serializable {
 
     /**
      * Returns the child node at the given index (for non-token nodes only).
-     * 
-     * @param index     the desired child node's index
-     * @return          the child node at the given index
-     * @throws UnsupportedOperationException    if this node is a token node
+     *
+     * @param index the desired child node's index
+     *
+     * @return the child node at the given index
+     *
+     * @throws UnsupportedOperationException if this node is a token node
      */
     public ParseNode getChild(int index) {
         if (this.isToken) {
             throw new UnsupportedOperationException("Call to getChild for token node " + this.name);
-        } else {
-            return (ParseNode) this.children.get(index);
         }
+
+        return (ParseNode) this.children.get(index);
     }
 
     /**
      * Returns this node's parent node.
-     * 
-     * @return  this node's parent node, <code>null</code> if this node
-     *          is the root node 
+     *
+     * @return this node's parent node, <code>null</code> if this node is the root node
      */
     public ParseNode getParent() {
         return this.parent;
     }
 
     /**
+     * Sets this node's parent node.
+     *
+     * @param parent the parse node that shall become this node's parent
+     */
+    public void setParent(ParseNode parent) {
+        this.parent = parent;
+    }
+
+    /**
      * Returns this node's lexeme (for token nodes only).
-     * 
-     * @return  this node's lexeme
-     * @throws UnsupportedOperationException    if this node is not a token node
+     *
+     * @return this node's lexeme
+     *
+     * @throws UnsupportedOperationException if this node is not a token node
      */
     public String getLexeme() {
-        if (this.isToken) {
-            return this.lexeme;
-        } else {
+        if (!this.isToken) {
             throw new UnsupportedOperationException();
         }
+
+        return this.lexeme;
     }
 
     /**
      * Returns this node's line number (for token nodes only).
+     * <p/>
      * Note that epsilon nodes have line number -2.
-     * 
-     * @return  this node's line number
-     * @throws UnsupportedOperationException    if this node is not a token node
+     *
+     * @return this node's line number
+     *
+     * @throws UnsupportedOperationException if this node is not a token node
      */
     public int getLineno() {
-        if (this.isToken) {
-            return this.lineno;
-        } else {
+        if (!this.isToken) {
             throw new UnsupportedOperationException();
         }
+
+        return this.lineNumber;
     }
 
     /**
      * Returns this node's line number if it is a token node,
      * and the line number of the leftmost token node reachable from
      * this node otherwise. Note that epsilon nodes have line number -2.
-     * 
-     * @return  a reasonable line number
+     *
+     * @return a reasonable line number
      */
     public int getLinenoLeft() {
         if (this.isToken) {
-            return this.lineno;
-        } else {
-            return this.getChild(0).getLinenoLeft();
+            return this.lineNumber;
         }
-    }
 
-    /**
-     * Searches the first ancestor that has more than one child and
-     * calls this ancestor's getLinenoLeft().
-     * 
-     * @return  a line number
-     */
-    /* DELME: doesn't work like that (infinite loop)
-    private int getNextLinenoLeft() {
-        ParseNode p = this.parent;
-        while (parent != null) {
-            if (p.getNumChildren() > 1) {
-                ParseNode c = p.getChild(1);
-                return c.getLinenoLeft();
-            }
-        }
-        return -2;
+        return this.getChild(0).getLinenoLeft();
     }
-    */
 
     /**
      * Returns this node's ID.
-     * 
-     * @return  this node's ID
+     *
+     * @return this node's ID
      */
     public int getId() {
         return this.id;
     }
 
-    /** 
+    /**
      * Returns true if this node is a token node.
      *
-     * @return  <code>true</code> if this node is a token node, 
-     *          <code>false</code> otherwise
+     * @return <code>true</code> if this node is a token node, <code>false</code> otherwise
      */
     public boolean isToken() {
         return this.isToken;
     }
 
-// SET *****************************************************************************    
-
     /**
-     * Sets this node's parent node.
-     * 
-     * @param parent  the parse node that shall become this node's parent
-     */
-    public void setParent(ParseNode parent) {
-        this.parent = parent;
-    }
-
-// OTHER ***************************************************************************
-    
-    /**
-     * Adds a node to this node's children and makes this node the 
-     * given node's parent (for non-token nodes only).
-     * 
-     * @param child  the parse node that shall become this node's child
-     * @throws UnsupportedOperationException    if this node is a token node
+     * Adds a node to this node's children and makes this node the given node's parent (for non-token nodes only).
+     *
+     * @param child the parse node that shall become this node's child
+     *
+     * @throws UnsupportedOperationException if this node is a token node
      */
     public ParseNode addChild(ParseNode child) {
         if (this.isToken) {
             throw new UnsupportedOperationException();
-        } else {
-            this.children.add(child);
-            child.setParent(this);
-            return child;
         }
+
+        this.children.add(child);
+        child.setParent(this);
+
+        return child;
     }
 }
-
-
--- src/project/at/ac/tuwien/infosys/www/phpparser/ParseTree.java
+++ src/project/at/ac/tuwien/infosys/www/phpparser/ParseTree.java
@@ -1,6 +1,6 @@
 /*
  * ParseTree.java
- * 
+ *
  * Copyright (C) 2005 Nenad Jovanovic
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,66 +18,59 @@
  *
  */
 
-
 package at.ac.tuwien.infosys.www.phpparser;
 
-import java.util.*;
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Tree constructed by the parser.
- * 
- * @author  Nenad Jovanovic
- * @version July 14, 2005
+ *
+ * @author Nenad Jovanovic
  */
-public final class ParseTree
-implements Serializable {
-
-    /** root node of the parse tree */
+public final class ParseTree implements Serializable {
+    /**
+     * root node of the parse tree
+     */
     private final ParseNode root;
-
-// CONSTRUCTORS ********************************************************************
 
     /**
      * Constructs a parse tree for the given root node.
-     * 
-     * @param root  the root node for the parse tree
+     *
+     * @param root the root node for the parse tree
      */
     public ParseTree(ParseNode root) {
         this.root = root;
-    } 
+    }
 
-// GET *****************************************************************************
-    
     /**
      * Returns this parse tree's root node.
-     * 
-     * @return  this parse tree's root node
+     *
+     * @return this parse tree's root node
      */
     public ParseNode getRoot() {
         return this.root;
-    } 
-    
-// OTHER ***************************************************************************
-    
+    }
+
     /**
      * Returns an <code>Iterator</code> over the leaf parse nodes of this parse tree
-     * 
-     * @return      an <code>Iterator</code> over the leaf parse nodes
+     *
+     * @return an <code>Iterator</code> over the leaf parse nodes
      */
     public Iterator leafIterator() {
         LinkedList list = new LinkedList();
         this.leafIteratorHelper(list, this.root);
+
         return list.iterator();
     }
 
-
     /**
      * Helper method for <code>leafIterator</code>.
-     * 
-     * @param list  the list to which to add leaf nodes
-     * @param node  root node of the tree for which the leaf nodes shall be
-     *              added to the given list
+     *
+     * @param list the list to which to add leaf nodes
+     * @param node root node of the tree for which the leaf nodes shall be added to the given list
      */
     private void leafIteratorHelper(List list, ParseNode node) {
         // if we've reached a leaf
@@ -85,11 +78,11 @@ implements Serializable {
             list.add(node);
             return;
         }
+
         // handle successors
-        for (Iterator iter = node.getChildren().iterator(); iter.hasNext(); ) {
-            ParseNode child = (ParseNode) iter.next();
+        for (Iterator iterator = node.getChildren().iterator(); iterator.hasNext(); ) {
+            ParseNode child = (ParseNode) iterator.next();
             leafIteratorHelper(list, child);
         }
     }
 }
-
